f470a1c29c2d70a8a501ce50f8be5d3b0e881644
Simon <106246464+SimonnBunvent@users.noreply.github.com>
2024-11-27 20:03:17 +0100

    changement des nom du template pour faire fonctionner le site web

--- app/views/artists.py
+++ app/views/artists.py
@@ -1,12 +1,9 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from app.utils import *
 
-# Define the Blueprint for 'artists'
 artists_bp = Blueprint('artists', __name__, url_prefix='/artists')
 
-# Define the route for '/artists/ppl'
 @artists_bp.route('/')
 
 def people():
     return render_template('artists/people.html')
-def
