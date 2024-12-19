669b503d890c75568ef6f4847e3bfcc7329f31f2
Simon <106246464+SimonnBunvent@users.noreply.github.com>
2024-12-19 15:35:25 +0100

    fini 90% page "create"

--- app/views/create.py
+++ app/views/create.py
@@ -7,3 +7,7 @@ create_bp = Blueprint('create', __name__, url_prefix='/create')
 @create_bp.route('/')
 def createproject():
     return render_template('create/createproject.html')
+
+@create_bp.route('/send')
+def sendbp():
+    return render_template('create/send.html')
